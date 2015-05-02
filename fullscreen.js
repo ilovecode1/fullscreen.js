435362c30566dd25b17d8c50d14d0ad3713dcd0e
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-05-02 07:19:16 -0400

    Create fullscreen.js

---
+++ fullscreen.js
@@ -0,0 +1,23 @@
+//Fullscreen.js v1.0.0
+
+function fullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
